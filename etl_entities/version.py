474a3faf7bf8a30a281ae8442203267cfb742446
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2021-12-13 11:21:21 +0300

    [ONE-219] Fix flake8 error

--- etl_entities/version.py
+++ etl_entities/version.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 VERSION_FILE = Path(__file__).parent / "VERSION"
 
-__version__ = VERSION_FILE.read_text().strip()
+__version__ = VERSION_FILE.read_text().strip()  # noqa: WPS410
