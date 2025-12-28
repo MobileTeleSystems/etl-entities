97e1841146fd886073ecca41a3bbefb2675e3721
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-28 15:32:59 +0300

    Fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,14 +4,14 @@ set -e
 
 root_path=$(dirname $(realpath $0))
 
-python3 -m pip uninstall -y dummy failing || true
-python3 -m pip install -e $root_path/tests/libs/dummy
-python3 -m pip install -e $root_path/tests/libs/failing
+pip uninstall -y dummy failing || true
+pip install -e $root_path/tests/libs/dummy
+pip install -e $root_path/tests/libs/failing
 
 set +e
 
 $root_path/pytest_runner.sh "$@"
 ret=$?
 
-python3 -m pip uninstall -y dummy failing || true
+pip uninstall -y dummy failing || true
 exit "$ret"
